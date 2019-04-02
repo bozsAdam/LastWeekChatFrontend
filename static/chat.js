cb36de467ce365861051c2a3657e3434ed97a98b
Adam Bozsoki <09.adam.b@gmail.com>
2019-04-02 13:44:32 +0200

    Add function to connect to socket

---
+++ static/chat.js
@@ -0,0 +1,14 @@
+let serverUrl = "http://localhost:61000/";
+
+function connectToSocket() {
+    var socket = new SockJS(serverUrl + 'chat-server');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/chat', function (greeting) {
+            console.log("Connected to the game socket");
+            console.log(JSON.parse(greeting.body))
+        });
+
+    });
+}
